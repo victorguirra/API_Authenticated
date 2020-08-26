3f887e62113af26686c695f5b9c497c9d63bbe86
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-26 15:57:46 -0300

    Update Return Messages

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ Message: "Using GET Method!" })
+    res.status(200).json({ Message: "Showing All Products!" })
 })
 
 router.post('/', (req, res) => {
-    res.status(200).json({ Message: "Using POST Method!" })
+    res.status(200).json({ Message: "Add New Product!" })
 })
 
 router.get('/:id', (req, res) => {
-    res.status(200).json({ Message: `Return Product With Id: ${ req.params.id }` })
+    res.status(200).json({ Message: `Show Single Product With Id: ${ req.params.id }` })
 })
 
-router.put('/', (req, res) => {
-    res.status(200).json({ Message: "Using PATCH Method!" })
+router.put('/:id', (req, res) => {
+    res.status(200).json({ Message: "Update A Single Product!" })
 })
 
-router.delete('/', (req, res) => {
-    res.status(200).json({ Message: "Using DELETE Method!" })
+router.delete('/:1', (req, res) => {
+    res.status(200).json({ Message: "Delete a Product!" })
 })
 
 module.exports = router;    
