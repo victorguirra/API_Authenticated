2f07230788d83ec373257ff8c5593ccf56b68ce5
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-26 16:26:14 -0300

    Configured Error Messages

--- src/server.js
+++ src/server.js
@@ -3,8 +3,25 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+
 app.use('/products', require('./routes/products'));
 app.use('/requests', require('./routes/requests'));
+
+app.use(( req, res, next ) => {
+    const error  = new Error("No Route Found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+
+    return res.send({
+        newError:{
+            message: error.message
+        }
+    })
+})
 
 app.listen(3333, () => {
     console.log("✔ Server Running at URL: http://localhost:3333");
