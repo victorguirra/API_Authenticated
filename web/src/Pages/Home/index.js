e72adf7be3eaa656a46cec2dd9b338abf085488b
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-29 19:41:38 -0300

    Config Use Params in Home Page

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../Services/api';
 
 import { Container } from './styles';
 
 export default function Home(){
+    const { id } = useParams();
+
+    const [ userData, setUserData ] = useState([]);
+    
+    useEffect(() => {
+
+        async function FetchData(){
+            const allUsersData = await api.get('/users');
+
+            const findUserData = allUsersData.data.find( user => user._id === id);
+
+            await setUserData(findUserData);
+        }
+
+        FetchData();
+
+    }, [])
+
     return(
         <Container>
 
-            <h1>Here Go Home Page</h1>
+            <h1>Seja Muito Bem-Vindo(a) {userData.name}</h1>
 
         </Container>
     )
