4cd43cea48bd3c569cdea2102395131df411365c
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-29 12:42:52 -0300

    Added Cors In Server

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 require('./database/connection');
 
