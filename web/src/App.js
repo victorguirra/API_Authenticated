9e2b751766cbe8cffbc7115439d9da47a4852400
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-08-29 14:05:38 -0300

    Created GlobalStyles

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import GlobalStyle from './GlobalStyles';
+
 export default function App() {
   return (
     <div className="App">
-      
+      <GlobalStyle />
       <h1>Hello World</h1>
     </div>
   );
---
+++ web/src/GlobalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
+
+    
+
+    *{
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+    }
+
+    body, input, button{
+        font-family:'Oswald', sans-serif;
+        
+        --purple: #8357E5;
+        --green: ##04D362; 
+    }
+
+`;
+
+export default GlobalStyle;
